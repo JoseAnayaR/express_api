bb15e182c3091edbfe46293cbfb2d6cb6b8ca492
JoseAnayaR <joseanayautj@gmail.com>
2022-05-13 14:27:53 -0500

    1.7 Agregando endpoint que se encargue de modificar un explorer 'PUT'

--- app.js
+++ app.js
@@ -32,6 +32,15 @@ app.post('/v1/explorers', (req, res) => {
     res.status(201).json({message: "Created"})
 })
 
+//PUT Crear un endpoint que se encargue de actualizar un explorer
+app.put('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers Put request ${new Date()}`)
+    console.log(`Update explorer with id ${req.params.id}`)
+    const requestBody = req.body //Parámetros de un cliente
+    res.status(200).json({message: "Updated"})
+})
+
+
 
 // Con esto inicializamos esta app
 app.listen(port, () => {
